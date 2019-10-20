d014dbe71d40fa9f0062d6dae66426f96fbe9909
Ryan Floyd <arfloyd7@gmail.com>
2019-10-20 19:47:10 -0400

    First Node Express App - Mini Message Board

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+const messages = [ 
+  {text: 'Hi there!', user: 'Amando', added: new Date() }, 
+  { text: 'Hello World!', user: 'Charles', added: new Date() } 
+];
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Message Board',
+  'messages': messages});
+});
+
+router.get('/new', (err, res) => {
+  res.render('form', {title: "Add a new Message"});
+})
+
+router.post("/new", (req, res) => {
+  messages.push({text: req.body.message, user: req.body.name, added: new Date()});
+  res.redirect("/");
+});
+
+
+
+module.exports = router;
